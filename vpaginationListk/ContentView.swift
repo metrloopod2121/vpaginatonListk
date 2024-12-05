d26c1c88597e09d9bd22ed8095bb8ecede298ecf
Matvey <metrloopod2121@mail.ru>
2024-12-05 22:27:35 +0300

    Initial Commit

---
+++ vpaginationListk/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  vpaginationListk
+//
+//  Created by 𝕄𝕒𝕥𝕧𝕖𝕪 ℙ𝕠𝕕𝕘𝕠𝕣𝕟𝕚𝕪 on 05.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ vpaginationListk/vpaginationListkApp.swift
@@ -0,0 +1,17 @@
+//
+//  vpaginationListkApp.swift
+//  vpaginationListk
+//
+//  Created by 𝕄𝕒𝕥𝕧𝕖𝕪 ℙ𝕠𝕕𝕘𝕠𝕣𝕟𝕚𝕪 on 05.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct vpaginationListkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
